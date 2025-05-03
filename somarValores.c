2c5d6a0d81da40c3ff9342c797a6894a199b940b
Ray-Nunes <raybnunes08@gmail.com>
2025-05-03 14:49:10 -0300

    primeiro commit

---
+++ somarValores.c
@@ -0,0 +1,19 @@
+int main(){
+
+    int resultado = 0, n1 = 200, n2 = 1;
+
+    while(n1 >= 1){
+       
+        if((200 == 200)){
+
+            printf("\n%d + %d = %d", n1, n2, n1+n2);
+            resultado += n1+n2;
+            n1--;
+            n2++;
+        }
+       
+
+    }
+    printf("\n\nResultado: %d", resultado/2);
+    
+}
